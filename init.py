6c1477935392a4f489d3038aea09ba5200b90b4d
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2025-02-20 08:20:15 +0700

    Create init.py

---
+++ init.py
@@ -0,0 +1,18 @@
+# __init__.py
+
+import os
+import logging
+from dotenv import load_dotenv
+
+# Memuat variabel dari file .env
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+OWNER_ID = os.getenv("OWNER_ID")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+MONGO_URI = os.getenv("MONGO_URI")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
