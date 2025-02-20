78f920c6f1fcbe32a524fd89a8c108d87cd60ff9
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2025-02-20 10:15:50 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Konfigurasi MetaTrader 5
+MT5_LOGIN = 12345678  # Ganti dengan login akun MT5 Anda
+MT5_PASSWORD = "your_mt5_password"  # Ganti dengan password akun MT5 Anda
+MT5_SERVER = "Exness-MT5Real"  # Ganti dengan server broker Anda
+
+# Konfigurasi Telegram
+TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
+TELEGRAM_CHAT_ID = "your_channel_or_chat_id"
+
+# Konfigurasi MongoDB
+MONGO_URI = "your_mongodb_uri"
+DATABASE_NAME = "trading_bot_db"
